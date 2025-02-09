0186cf7fc905cf7cf78da63c6f142740fb178cb3
Ananta <codeantu@gmail.com>
2025-02-09 16:45:58 +0530

    Refactor test route component to remove Firebase token retrieval; add post and get request functionality

--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -1,40 +1,30 @@
 import { Button } from '@/components/ui/button';
-import app from '@/firebase/firebaseConfig';
 import { createFileRoute } from '@tanstack/react-router';
 import axios from 'axios';
-import { getAuth } from 'firebase/auth';
-import { useEffect, useState } from 'react';
 
 export const Route = createFileRoute('/test')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [token, setToken] = useState<string | null>(null);
-  const auth = getAuth(app);
+  
+  const formData = new FormData();
+  formData.append('name', 'test');
 
-  async function getToken() {
-    console.log('getToken');
-
-    try {
-      const user = auth.currentUser;
-      if (user) {
-        const token = await user.getIdToken(); // 🔥 Firebase ID Token
-        setToken(token);
-      }
-      return null;
-    } catch (error) {
-      console.error(error);
-    }
+  async function postTest() {
+    const res = await axios.post('http://localhost:3000/api/test', formData);
+    console.log(res);
   }
 
-
-
-  console.log(token);
+  async function test() {
+    const res = await axios.get('http://localhost:3000/api/test');
+    console.log(res);
+  }
 
   return (
     <div>
-      <Button onClick={getToken}>Get Token</Button>
+      <Button onClick={test}>Get Token</Button>
+      <Button onClick={postTest}>Post Test</Button>
       {/* <Button onClick={test}>Test</Button> */}
     </div>
   );
