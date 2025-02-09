58e3f5f514072504ff531d2678d011dc3116cd33
Ananta <codeantu@gmail.com>
2025-02-09 18:02:28 +0530

    Add Zod validation and enhance test route with authentication functionality

--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -1,15 +1,23 @@
 import { Button } from '@/components/ui/button';
 import { createFileRoute } from '@tanstack/react-router';
 import axios from 'axios';
+import { z } from 'zod';
 
 export const Route = createFileRoute('/test')({
   component: RouteComponent,
 });
 
+export const testValidator = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  age: z.string().nonempty(),
+});
+
 function RouteComponent() {
-  
   const formData = new FormData();
-  formData.append('name', 'test');
+  formData.append('name', 'John Doe');
+  formData.append('email', 'test@gmail.com');
+  formData.append('age', '25');
 
   async function postTest() {
     const res = await axios.post('http://localhost:3000/api/test', formData);
@@ -21,10 +29,22 @@ function RouteComponent() {
     console.log(res);
   }
 
+  async function auth() {
+    const res = await axios.get('http://localhost:3000/api/auth');
+    console.log(res);
+  }
+
+  async function authPost() {
+    const res = await axios.post('http://localhost:3000/api/auth', formData);
+    console.log(res);
+  }
+
   return (
     <div>
       <Button onClick={test}>Get Token</Button>
       <Button onClick={postTest}>Post Test</Button>
+      <Button onClick={auth}>Auth</Button>
+      <Button onClick={authPost}>Auth Post</Button>
       {/* <Button onClick={test}>Test</Button> */}
     </div>
   );
---
+++ src/rpc/test.ts
@@ -0,0 +1,7 @@
+
+// import { AppType } from '.'
+// import { hc } from 'hono/client'
+
+// const client = hc<AppType>('http://localhost:3000/')
+
+// export default client
