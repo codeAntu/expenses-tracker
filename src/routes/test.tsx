db3ac78a2b52ff6025506abd3857178b78019f6e
Abinash Karmakar <codeAbinash@gmail.com>
2025-02-10 19:34:55 +0530

    Fix CROS error

--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -1,21 +1,20 @@
 import { Button } from '@/components/ui/button';
-import { createFileRoute } from '@tanstack/react-router';
 import client from '@/utils/client';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/test')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  async function testClient() {
-    const res = await client.api.$get();
-    console.log(res);
+  async function handleFetch() {
+    const data = await (await client.api.hello.$get()).json();
+    console.log(data);
   }
 
   return (
     <div>
-      <Button onClick={testClient}>Test Client</Button>
-      {/* <Button onClick={test}>Test</Button> */}
+      <Button onClick={handleFetch}>Test Client</Button>
     </div>
   );
 }
--- src/rpc/hcWithType.d.ts
+++ src/rpc/hcWithType.d.ts
@@ -1,16 +1,17 @@
 import { hc } from "hono/client";
 declare const client: {
-    api: import("hono/client").ClientRequest<{
-        $get: {
-            input: {};
-            output: {
-                message: string;
-                status: boolean;
+    api: {
+        hello: import("hono/client").ClientRequest<{
+            $get: {
+                input: {};
+                output: {
+                    message: string;
+                };
+                outputFormat: "json";
+                status: import("hono/utils/http-status").ContentfulStatusCode;
             };
-            outputFormat: "json";
-            status: import("hono/utils/http-status").ContentfulStatusCode;
-        };
-    }>;
+        }>;
+    };
 };
 export type Client = typeof client;
 export declare const hcWithType: (baseUrl: string, options?: import("hono").ClientRequestOptions | undefined) => Client;
