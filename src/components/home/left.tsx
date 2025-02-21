254ab4899ba462ca876e7f9aabc2fc757afbac9f
Ananta <codeantu@gmail.com>
2025-02-22 00:16:35 +0530

    Add Progress component; update dependencies in package.json and bun.lock; enhance Left component with Goals section and progress indicators

--- src/components/home/left.tsx
+++ src/components/home/left.tsx
@@ -1,11 +1,15 @@
 import { Tilt } from '@/components/ui/tilt';
 import { useEffect, useState } from 'react';
 import { AnimatedNumber } from '../ui/animated-number';
+import Box from '../box';
+import { Home } from 'lucide-react';
+import { Progress } from '@/components/ui/progress';
 
 export default function Left() {
   return (
-    <div className='flex-1 border-r p-5 '>
+    <div className='flex flex-col gap-5 border-r'>
       <TiltCard1 />
+      <Goals />
     </div>
   );
 }
@@ -18,11 +22,11 @@ export function TiltCard1() {
   }, []);
 
   return (
-    <Tilt rotationFactor={1} isRevese className='flex items-center justify-center'>
-      <div className='bg-accent border-foreground/5 inline-flex aspect-video w-full max-w-[500px] flex-col items-center justify-center overflow-hidden rounded-xl border p-5'>
-        <div className='flex h-full w-full flex-col justify-center'>
+    <Tilt rotationFactor={1} isRevese className='flex items-center justify-center border-b p-8'>
+      <div className='bg-accent border-foreground/5 aspect-video w-full max-w-[400px] min-w-[340px] sm:min-w-[400px] flex-col items-center justify-center overflow-hidden rounded-xl border p-5'>
+        <div className='flex h-full w-full flex-col justify-start'>
           <div className='text-accent-foreground/50 text-xl font-bold'>Total Money</div>
-          <div className='flex items-center justify-center space-x-2 px-10 py-10 text-zinc-800 dark:text-zinc-50'>
+          <div className='flex h-full items-center justify-center space-x-2 px-10 py-10 text-zinc-800 dark:text-zinc-50'>
             <div className='inline-flex items-center font-mono text-5xl font-bold'>
               <span>&#8377;</span>
               <AnimatedNumber
@@ -35,9 +39,61 @@ export function TiltCard1() {
               />
             </div>
           </div>
-          <div className='text-accent-foreground/50 text-xl font-bold'></div>
+          {/* <div className='text-accent-foreground/50 text-xl font-bold'></div> */}
         </div>
       </div>
     </Tilt>
   );
 }
+
+const Goals = () => {
+  return (
+    <div className='grid gap-3 border-l px-3 sm:p-7 pt-4 pb-6'>
+      <div className='flex items-center justify-between px-1'>
+        <div className='text-secondary-foreground/90 flex items-center gap-2 pb-0.5 text-lg font-semibold'>Goals</div>
+        <div className='rounded-sm px-4 py-1 text-sm font-semibold text-blue-500 hover:bg-blue-500/10 hover:text-blue-500'>
+          See All
+        </div>
+      </div>
+      <div className='flex flex-col gap-3.5'>
+        {/* {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))} */}
+        <Goal />
+        <Goal />
+        <Goal />
+        <Goal />
+      </div>
+    </div>
+  );
+};
+
+const Goal = () => {
+  const [progress, setProgress] = useState(13);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Box className='space-y-3'>
+      <div className='flex w-full items-center justify-between'>
+        <div className='flex items-center gap-3'>
+          <Home className='rounded-2xl bg-blue-400/50 p-3' size={50} />
+          <div>
+            <div className='text-accent-foreground/90 text-base font-semibold'>Name </div>
+            <div className='text-accent-foreground/70 line-clamp-1 text-xs font-semibold'>Description</div>
+          </div>
+        </div>
+        <div className='flex flex-col items-end'>
+          <div className='text-sm font-semibold text-red-500/80'>12 days left</div>
+          <div className='text-accent-foreground/70 text-xsm font-semibold'>3000 / 4000</div>
+        </div>
+      </div>
+      <div className='w-full'>
+        <Progress value={progress} className='rounded-2xl' />
+      </div>
+    </Box>
+  );
+};
--- src/components/home/right.tsx
+++ src/components/home/right.tsx
@@ -1,7 +1,6 @@
 import Box from '@/components/box';
 import { Calendar as Cal } from '@/components/ui/calendar';
 import { useState } from 'react';
-import Transactions from '../transactions';
 
 function Right() {
   const [date, setDate] = useState<Date>(new Date());
@@ -9,7 +8,7 @@ function Right() {
   const today = new Date();
 
   return (
-    <div className='bg-red-50w flex h-full max-w-96 flex-col gap-1 self-center border-l sm:max-w-none sm:flex-1 sm:flex-row xl:max-w-[350px] xl:flex-col'>
+    <div className='bg-red-50w flex h-full max-w-96 flex-col gap-1 self-center border-l sm:max-w-none sm:flex-1 sm:flex-row xl:flex-col'>
       <div className='flex flex-col justify-center gap-3 border-b p-5 pb-6'>
         <div className='flex items-center justify-between px-1'>
           <div className='text-secondary-foreground/90 flex items-center gap-2 pb-0.5 text-lg font-semibold'>
@@ -25,24 +24,27 @@ function Right() {
             Today
           </button>
         </div>
-        <Box className='space-y-10 p-1'>
-          <Cal
-            key={key}
-            mode='single'
-            selected={date}
-            onSelect={(day) => day && setDate(day)}
-            className='flex h-full w-full items-center justify-center rounded-md'
-            classNames={{
-              months: 'w-full  min-w-[200px]  ',
-              month: 'space-y-4 w-full flex flex-col ',
-              table: 'w-full h-full border-collapse  space-y-10 ',
-              head_row: ' space-x-10',
-              row: 'w-full mt-2 ',
-            }}
-          />
-        </Box>
+        <div className='flex justify-baseline border'>
+          {/* <div className='rotate-[270deg] transform text-7xl font-extrabold bg-red-200'>12Feb</div> */}
+          <Box className='space-y-10 bg-amber-100 p-1'>
+            <Cal
+              key={key}
+              mode='single'
+              selected={date}
+              onSelect={(day) => day && setDate(day)}
+              // className='flex h-full w-full items-center justify-center rounded-md'
+              // classNames={{
+              //   months: 'w-full  min-w-[200px]  ',
+              //   month: 'space-y-4 w-full flex flex-col ',
+              //   table: 'w-full h-full border-collapse  space-y-10 ',
+              //   head_row: ' space-x-10',
+              //   row: 'w-full mt-2 ',
+              // }}
+            />
+          </Box>
+        </div>
       </div>
-      <Transactions date={date} />
+      {/* <Transactions date={date} /> */}
     </div>
   );
 }
