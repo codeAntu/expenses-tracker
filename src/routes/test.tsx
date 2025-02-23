6b6a5ef8e88cfc5e35f3582a9b93f1efe0b274ae
Ananta <codeantu@gmail.com>
2025-02-23 11:33:13 +0530

    refactor :  firebase login

--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -4,14 +4,37 @@ import client from '@/utils/client';
 import { createFileRoute } from '@tanstack/react-router';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { Home } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 export const Route = createFileRoute('/test')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [user, setUser] = useState<unknown>();
+  const [idToken, setIdToken] = useState<string>('');
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
+
+  console.log(user);
+  console.log(typeof user);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        getToken(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
+  async function getToken(user: any) {
+    if (!user) return;
+    const idToken = await user.getIdToken();
+    setIdToken(idToken);
+  }
 
   async function handleFetch() {
     const data = await (await client.api.hello.$get()).json();
@@ -52,10 +75,11 @@ function RouteComponent() {
     try {
       const result = await signInWithPopup(auth, provider);
       const user = result.user;
+      console.log('User:', user);
       const idToken = await user.getIdToken();
+      setIdToken(idToken);
       console.log('ID Token:', idToken);
 
-      // Send the token to the backend
       const response = await client.api.auth.$post({
         form: {
           idToken: idToken,
@@ -68,6 +92,25 @@ function RouteComponent() {
     }
   }
 
+  async function logout() {
+    try {
+      await auth.signOut();
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
+  }
+
+  async function sendIdToken() {
+    const response = await client.api.auth.$post({
+      form: {
+        idToken: idToken,
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <div>
       <Button onClick={handleFetch}>Test Client</Button>
@@ -75,8 +118,14 @@ function RouteComponent() {
       <Button onClick={text}>Test Fetch</Button>
       <Button onClick={testPost}>Test Post</Button>
       <Button onClick={transaction}>Test Transaction</Button>
-      <Button onClick={authFun}>Test New</Button>
+
+      {user ? <Button onClick={logout}>Logout </Button> : <Button onClick={authFun}>Login</Button>}
       <Home size={20} />
+      {idToken && (
+        <div>
+          <Button onClick={sendIdToken}>Send ID Token</Button>
+        </div>
+      )}
     </div>
   );
 }
