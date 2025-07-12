6d71ce8994681882a6b055f3acb286eeaf54a43b
codeantu <codeantu@gmail.com>
2025-07-12 20:42:32 +0530

    fix: add logging for auth token in setAuthToken function

--- src/query/exe.ts
+++ src/query/exe.ts
@@ -2,6 +2,8 @@ import { getCookie } from '@/utils/cookies';
 
 export function setAuthToken(t?: string): void {
   const token = getCookie('token') || t;
+  console.log('token', token);
+
   // have to set the token in the headers
 }
 
